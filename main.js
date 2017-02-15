12af6a28066337af03f5f67103c3883375520976
Daniel Amkaer Sorensen <daniel.amkaer@gmail.com>
2017-02-15 14:29:17 +0000

    added some preliminary admin interface

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
+var Sequelize = require('sequelize');
 var express = require('express');
 var app = express()
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
-var mongo = require('mongodb').MongoClient;
+var bodyParser = require('body-parser');
 
 var CONFIG = {
     updatePeriod: 15000,
 };
 
+
+var sequelize = new Sequelize('beer','','', { dialect: 'sqlite', storage: 'database.sqlite' });
+
+var Beer = sequelize.define('beer', {
+    name: {
+        type: Sequelize.STRING
+    },
+    slug: {
+        type: Sequelize.STRING
+    },
+    stock: {
+        type: Sequelize.INTEGER
+    },
+    price: {
+        type: Sequelize.FLOAT
+    },
+    min: {
+        type: Sequelize.FLOAT
+    },
+    max: {
+        type: Sequelize.FLOAT
+    },
+});
+
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use('/node_modules', express.static('node_modules'));
 app.set('view engine', 'jade');
 
@@ -17,22 +42,6 @@ http.listen(3000, function () {
     console.log('Listening on port 3000');
 });
 
-var Beer = function(name, slug, stock, price, min, max) {
-    this.name = name;
-    this.slug = slug;
-    this.stock = stock;
-    this.price = price;
-    this.min = min;
-    this.max = max;
-
-    this.data = [];
-}
-
-function db(callback) {
-    mongo.connect("mongodb://172.17.0.2:27017/beer", function(err, conn) {
-        callback(err, conn);
-    });
-}
 
 function getLast(dataSet, n) {
     if (n === undefined) n = 10;
@@ -44,28 +53,42 @@ function getLast(dataSet, n) {
     return out;
 }
 
-var beers = {
-    tuborg: new Beer('Tuborg Pilsner', 'tuborg', 300, 10.0, 4.0, 100.0),
-    classic: new Beer('Tuborg Classic', 'classic', 300, 10.0, 4.0, 100.0),
-    carlsberg: new Beer('Carlsberg', 'carlsberg', 300, 10.0, 4.0, 100.0),
-    raw: new Beer('Tuborg Rå', 'raw', 300, 10.0, 4.0, 100.0),
-    lager: new Beer('Carlsberg Lager', 'lager', 300, 10.0, 4.0, 100.0),
-    draft: new Beer('Fadøl', 'draft', 300, 10.0, 4.0, 100.0),
-    drink: new Beer('Drink', 'drink', 300, 10.0, 4.0, 100.0),
-};
 
-io.on('connection', function(socket) {
-    socket.emit('beers', beers);
-});
+// io.on('connection', function(socket) {
+//     socket.emit('beers', beers);
+// });
 
 app.get('/', function (req, res) {
-    res.render('index', {
-        beers: beers,    
+    Beer.findAll().then((beers) => {
+        res.render('index', {
+            beers: beers,    
+        });
     });
 });
 
-function PeriodicUpdate() {
-    console.log('Running periodic update.');
-}
+app.post('/admin', function (req, res) {
+    Beer.findAll().then((beers) => {
+        beers.forEach((beer) => {
+            beer.name = req.body["name"][beer.slug];
+            beer.stock = req.body["stock"][beer.slug];
+            beer.price = req.body["price"][beer.slug];
+            beer.min = req.body["min"][beer.slug];
+            beer.max = req.body["max"][beer.slug];
+            beer.save()
+        });
+    });
 
-setInterval(PeriodicUpdate, CONFIG.updatePeriod);
+    if (res.body["slug"]["NEW"] != "") {
+
+    }
+
+    res.send('OK');
+});
+
+app.get('/admin', function (req, res) {
+    Beer.findAll().then((beers) => {
+        res.render('admin', {
+            beers: beers,    
+        });
+    });
+});
